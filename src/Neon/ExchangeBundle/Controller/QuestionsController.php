46d2d8b5bb7ffddc9f6691b8e373bb5f7895f60a
David Yell <dyell@ukwebmedia.com>
2013-06-28 14:24:54 +0100

    Created the add question stack

--- src/Neon/ExchangeBundle/Controller/QuestionsController.php
+++ src/Neon/ExchangeBundle/Controller/QuestionsController.php
@@ -4,16 +4,62 @@ namespace Neon\ExchangeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Neon\ExchangeBundle\Form\Type\QuestionType;
 
 /**
  * Description of QuestionsController
  *
  * @author David Yell <neon1024@gmail.com>
+ *
+ * @Route("/")
  */
 class QuestionsController extends Controller {
 
+    /**
+     * List the questions
+     *
+     * @Route("/", name="latest_questions")
+     * @Template()
+     *
+     * @return array
+     */
     public function indexAction() {
-        return $this->render('NeonExchangeBundle:Questions:index.html.twig');
+        $questions = $this->getDoctrine()
+            ->getRepository('NeonExchangeBundle:Question')
+            ->findAll();
+
+        return array('questions' => $questions);
+    }
+
+    /**
+     * Add a new question
+     *
+     * @Route("/questions/add", name="add_question")
+     * @Template()
+     *
+     * @return array
+     */
+    public function addAction(Request $request) {
+        $form = $this->createForm(new QuestionType());
+
+        if ($request->isMethod('post')) {
+            $form->bind($this->getRequest());
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($form->getData());
+            $manager->flush();
+
+            $this->get('session')->getFlashBag()->add('notice', 'Question saved');
+            return $this->redirect($this->generateUrl('latest_questions'));
+        }
+
+        return array(
+            'form' => $form->createView()
+        );
     }
 
 }
---
+++ src/Neon/ExchangeBundle/Form/Type/QuestionType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Neon\ExchangeBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Description of QuestionType
+ *
+ * @author David Yell <neon1024@gmail.com>
+ */
+class QuestionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('title')
+            ->add('question')
+            ->add('user', 'entity', array(
+                'class' => 'NeonExchangeBundle:User',
+                'property' => 'email'
+            ));
+    }
+
+    public function getName() {
+        return 'add_question_form';
+    }
+
+    public function setDefaultOptions(\Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'Neon\ExchangeBundle\Entity\Question'
+        ));
+    }
+
+
+}
