3303fcd49faf8fd16f0cb7c39577c654d952ea94
David Yell <dyell@ukwebmedia.com>
2014-01-22 17:06:42 +0000

    Include KNP Paginator Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Neon\ExchangeBundle\NeonExchangeBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+			new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
