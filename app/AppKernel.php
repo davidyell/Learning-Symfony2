4c9b6ecdfab380aa160d3cce5756320d3423a282
David Yell <dyell@ukwebmedia.com>
2014-05-27 15:14:30 +0100

    Resolves #12 Removed the migrations bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Neon\ExchangeBundle\NeonExchangeBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 			new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle()
         );
