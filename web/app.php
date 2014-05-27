1b00966c3de6e941e53d7441c8fb6c635c89c8d3
David Yell <dyell@ukwebmedia.com>
2014-05-27 15:36:00 +0100

    Resolves #20

--- web/app.php
+++ web/app.php
@@ -4,11 +4,10 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
